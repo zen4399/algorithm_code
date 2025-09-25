55fc008731a09c2baf5c0beed225feab0f898a23
richard <isaozen4399@gmail.com>
2025-09-25 16:32:54 +0900

    test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import List, Tuple
-#from local_driver import Alg3D, Board # ローカル検証用
-from framework import Alg3D, Board # 本番用
+from local_driver import Alg3D, Board # ローカル検証用
+#from framework import Alg3D, Board # 本番用
 
 class MyAI(Alg3D):
     def get_move(
@@ -10,3 +10,9 @@ class MyAI(Alg3D):
         last_move: Tuple[int, int, int] # 直前に置かれた場所(x, y, z)
     ) -> Tuple[int, int]:
         # ここにアルゴリズムを書く
+        for y in range(4):
+            for x in range(4):
+                # 一番上(=z=3)まで埋まっていなければ置ける
+                if board[3][y][x] == 0:
+                    return (x, y)
+        return (0, 0)
